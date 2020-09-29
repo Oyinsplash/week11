a587de587a2b8b08f559e8c9e62496aa5e748169
Oyinsplash <alabi.oyinkansola14@outlook.com>
2020-09-29 23:40:02 +0100

    Added test for creating an organization

--- src/__test__/org.spec.ts
+++ src/__test__/org.spec.ts
@@ -24,7 +24,7 @@ describe("Test for Task 0ne", () => {
     done();
   });
 
-  it("Get one organizations in the database", async (done) => {
+  it("Get an organization in the database", async (done) => {
     request
       .post("/graphql")
       .send({
@@ -36,6 +36,20 @@ describe("Test for Task 0ne", () => {
         console.log("i got here");
         if (err) return done(err);
         console.log(res.body);
+        expect(res.body).toBeInstanceOf(Object);
+      });
+    done();
+  });
+
+  it("Add an organization to the database", async (done) => {
+    request
+      .post("/graphql")
+      .send({
+        mutation: 'updateOrganization(id: "5f67977f1b93d25dfb383e9c",organization: "Try",products: ["apps", "maps"],marketValue: "45",address: "7 Asajan street"ceo: "Dojcat",country: "Nigeria",employees: ["Shade", "cups", "tables", "John"]) {organization,employees}'})
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .end(function (err, res) {
+        if (err) return done(err);
         expect(res.body).toBeInstanceOf(Object);
       });
     done();
